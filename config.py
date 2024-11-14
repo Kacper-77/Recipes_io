762f4875eb55b708738bb47f8b77fc9ae4b8db31
Kacper-77 <ks.kontaktowy7@gmail.com>
2024-11-14 18:37:39 +0100

    fixes

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 
 def get_config():
-    config = st.secrets["redirect_uri"].startswith("https://recipes-io.streamlit.app")
+    config = st.secrets["production"] if st.secrets["redirect_uri"].startswith("https://recipes-io.streamlit.app") else st.secrets["development"]
     
     config["redirect_uri"] = config["redirect_uri"].rstrip('/')
-   
+    
     return config
