62ee7c598c712814a6b74ceff16f34d2be85affc
Kacper-77 <ks.kontaktowy7@gmail.com>
2024-11-15 18:06:34 +0100

    add users manage

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -5,6 +5,59 @@ from dotenv import load_dotenv
 from db import save_conversation, save_recipe
 import time
 from st_paywall import add_auth
+import psycopg2
+import pandas as pd
+from datetime import datetime, timezone
+
+
+@st.cache_resource
+def get_connection():
+    return psycopg2.connect(
+        dbname=st.secrets["database"],
+        user=st.secrets["username"],
+        password=st.secrets["password"],
+        host=st.secrets["host"],
+        port=st.secrets["port"],
+        sslmode=st.secrets["sslmode"]
+    )
+
+
+with get_connection() as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+CREATE TABLE IF NOT EXISTS usages (
+    id SERIAL PRIMARY KEY,
+    google_user_email VARCHAR(255),
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    output_tokens INTEGER,
+    input_tokens INTEGER,
+    input_text TEXT
+)
+        """)
+        conn.commit()
+
+
+def insert_usage(email, output_tokens, input_tokens, input_text):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO usages (google_user_email, output_tokens, input_tokens, input_text) VALUES (%s, %s, %s, %s)
+            """, (email, output_tokens, input_tokens, input_text))
+            conn.commit()
+
+
+def get_current_month_usage_df(email):
+    with get_connection() as conn:
+        now = datetime.now(timezone.utc)
+        start_date = datetime(now.year, now.month, 1)
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM usages WHERE google_user_email = %s AND created_at >= %s", (email, start_date))
+            rows = cur.fetchall()
+            columns = [desc[0] for desc in cur.description]
+            df_usage = pd.DataFrame(rows, columns=columns)
+
+    return df_usage
+
 
 # Inicjalizacja konfiguracji strony
 st.set_page_config(page_title="Recipes.io", layout="centered")
@@ -20,6 +73,14 @@ with st.sidebar:
 
     if st.session_state.get('email'):
         st.write(f"Zalogowano jako: {st.session_state['email']}")
+
+        usage_df = get_current_month_usage_df(st.session_state['email'])
+        st.write("W tym miesiącu użyłeś")
+        c0, c1 = st.columns([1, 1])
+        with c0:
+            st.metric("Input tokenów", usage_df['input_tokens'].sum())
+        with c1:
+            st.metric("Output tokenów", usage_df['output_tokens'].sum())
 
 try:
     add_auth(
@@ -62,12 +123,26 @@ if st.session_state.get('email'):
                 messages.append({"role": message["role"], "content": message["content"]})
 
             messages.append({"role": "user", "content": user_prompt})
-            return openai_client.chat.completions.create(
+            
+            # Wysyłanie zapytania do OpenAI
+            response = openai_client.chat.completions.create(
                 model="gpt-4o",
                 messages=messages,
                 stream=True,
-            )                  
-
+            )
+            
+            usage = response["usage"]
+            
+            # Zapisanie użycia w bazie danych
+            insert_usage(
+                email=st.session_state['email'],
+                output_tokens=usage['completion_tokens'],
+                input_tokens=usage['prompt_tokens'],
+                input_text=user_prompt,
+            )
+            
+            return response
+                  
         # Inicjalizacja stanu sesji dla konwersacji
         if "messages" not in st.session_state:
             st.session_state["messages"] = []
@@ -88,15 +163,17 @@ if st.session_state.get('email'):
         # Sekcja do wpisania nowego promptu przez użytkownika
         prompt = st.chat_input("W czym mogę ci pomóc?")
 
+        # Sekcja do obsługi promptu użytkownika
         if prompt:
-            # Dodanie wiadomości użytkownika do sesji
             user_message = {"role": "user", "content": prompt}
             with st.chat_message("user"):
                 st.markdown(user_message["content"])
 
             st.session_state["messages"].append(user_message)
 
+            # Otrzymanie odpowiedzi chatbota i zapisanie zużycia
             chatbot_response = get_chatbot_reply(prompt, memory=st.session_state["messages"][-10:])
+            
             with st.chat_message("assistant"):
                 assistant_message = st.write_stream(chatbot_response)
                 st.session_state["chatbot_reply"] = assistant_message
