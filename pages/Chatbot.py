5d7abc7077be8de8a02d63dc1488cd7b09dffffb
Kacper-77 <ks.kontaktowy7@gmail.com>
2024-11-20 17:12:44 +0100

    exp_v2

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -116,7 +116,7 @@ if st.session_state.get('email'):
                 input_text=user_prompt,
             )
 
-            return response
+            return response.choices[0].message.content
 
         # Inicjalizacja stanu sesji dla konwersacji
         if "messages" not in st.session_state:
