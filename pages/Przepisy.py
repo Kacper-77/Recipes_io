7c27a9791853d4d7ad5431dd2c9b493195ad0ce6
Kacper-77 <ks.kontaktowy7@gmail.com>
2024-11-23 10:11:04 +0100

    pages/przepisy.py_fix_v4

--- pages/Przepisy.py
+++ pages/Przepisy.py
@@ -3,10 +3,13 @@ from db import get_recipes, save_recipe, delete_recipe, init_db
 import time
 from st_paywall import add_auth
 
+
 init_db()
+
 
 st.title(":orange[Twoje przepisy] 🍜")
 
+# Sidebar z informacjami
 with st.sidebar:
     if st.session_state.get('email'):
         st.write(f"Zalogowano jako: {st.session_state['email']}")
@@ -18,9 +21,10 @@ with st.sidebar:
     with col2:
         st.link_button("Regulamin", "https://recipes-io-asstes.fra1.cdn.digitaloceanspaces.com/regulations.pdf")
     st.write("Kontakt: ks.kontaktowy7@gmail.com")
-    st.write("Podoba ci się aplikacja? wesprzyj nas link poniżej:")
+    st.write("Podoba ci się aplikacja? Wesprzyj nas link poniżej:")
     st.link_button("🥰", "https://buymeacoffee.com/kacperszaruga", use_container_width=True)
 
+# Obsługa logowania
 try:
     add_auth(
         required=False,
@@ -29,33 +33,37 @@ try:
 except KeyError:
     pass
 
+
 user_email = st.session_state.get('email')
 
-if st.session_state.get('email'):
+if user_email:
+    # Sekcja dodawania przepisu
     recipe_name = st.text_input("Nazwa przepisu")
     recipe_content = st.text_area("Treść przepisu")
     if st.button("Dodaj przepis"):
         if recipe_name and recipe_content:
             save_recipe(recipe_name, recipe_content, user_email)
             message = st.toast("Zapisano! 🎊")
-
             time.sleep(2)
-
             message.empty()
-
         else:
             st.warning("Proszę wprowadzić nazwę i treść przepisu.")
 
-    # Wyświetlanie zapisanych przepisów z bazy danych
+    # Sekcja wyświetlania zapisanych przepisów
     recipes = get_recipes(user_email)
     if recipes:
         for recipe in recipes:
-            with st.expander(recipe[1]):
-                st.write(recipe[2])
-                if st.button("Usuń", key=f"delete_{recipe[0]}"):
-                    delete_recipe(recipe[0])
-                    st.toast("Usunięto 🗑️")
-                    time.sleep(2)
-                    st.rerun()
+            try:
+                with st.expander(recipe[1]):
+                    st.write(recipe[2])
+                    if st.button("Usuń", key=f"delete_{recipe[0]}"):
+                        delete_recipe(recipe[0])
+                        st.toast("Usunięto 🗑️")
+                        time.sleep(2)
+                        st.rerun()
+            except IndexError:
+                st.error("Błąd podczas przetwarzania przepisu. Sprawdź dane.")
     else:
         st.write("Nie masz jeszcze żadnych przepisów.")
+else:
+    st.warning("Musisz być zalogowany, aby dodawać i przeglądać przepisy.")
