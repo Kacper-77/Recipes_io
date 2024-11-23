a49c1b9cd0dee4e0a5ac1791b0c379f4264ee99f
Kacper-77 <ks.kontaktowy7@gmail.com>
2024-11-23 10:06:40 +0100

    pages/przepisy.py_fix_v3

--- pages/Przepisy.py
+++ pages/Przepisy.py
@@ -29,11 +29,11 @@ try:
 except KeyError:
     pass
 
+user_email = st.session_state.get('email')
+
 if st.session_state.get('email'):
-    # Formularz dodawania nowego przepisu ręcznie
     recipe_name = st.text_input("Nazwa przepisu")
     recipe_content = st.text_area("Treść przepisu")
-    user_email = st.session_state.get('email')
     if st.button("Dodaj przepis"):
         if recipe_name and recipe_content:
             save_recipe(recipe_name, recipe_content, user_email)
@@ -47,17 +47,15 @@ if st.session_state.get('email'):
             st.warning("Proszę wprowadzić nazwę i treść przepisu.")
 
     # Wyświetlanie zapisanych przepisów z bazy danych
-    user_email = st.session_state.get('email')
-    if user_email:
-        recipes = get_recipes(user_email)
-        if recipes:
-            for recipe in recipes:
-                with st.expander(recipe[1]):  # recipe[1] to nazwa przepisu
-                    st.write(recipe[2])  # recipe[2] to treść przepisu
-                    if st.button("Usuń", key=f"delete_{recipe[0]}"):
-                        delete_recipe(recipe[0])
-                        st.toast("Usunięto 🗑️")
-                        time.sleep(2)
-                        st.rerun()
-        else:
-            st.write("Nie masz jeszcze żadnych przepisów.")
+    recipes = get_recipes(user_email)
+    if recipes:
+        for recipe in recipes:
+            with st.expander(recipe[1]):
+                st.write(recipe[2])
+                if st.button("Usuń", key=f"delete_{recipe[0]}"):
+                    delete_recipe(recipe[0])
+                    st.toast("Usunięto 🗑️")
+                    time.sleep(2)
+                    st.rerun()
+    else:
+        st.write("Nie masz jeszcze żadnych przepisów.")
